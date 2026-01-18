604472268ce4c466bc323feddd2f36611191b1fc
stratblues <tonystanell@gmail.com>
2026-01-18 13:18:00 -0500

    basic event loop for window

--- main.cpp
+++ main.cpp
@@ -9,14 +9,48 @@
 SDL_Window* window;
 SDL_GPUDevice* device;
 
+static int gameRunning;
+
+int initWindow()
+{
+	gameRunning = 1;
+ 	window = SDL_CreateWindow("Hello, Triangle!", 960, 540, SDL_WINDOW_RESIZABLE);
+	if(window == NULL)
+	{
+		//TODO: add logging
+		return NULL;
+	}
+        device = SDL_CreateGPUDevice(SDL_GPU_SHADERFORMAT_SPIRV, false, NULL);
+	if(device == NULL)
+	{
+		return NULL;
+	}
+
+        SDL_ClaimWindowForGPUDevice(device, window);	
+
+}
+
+
 int main
 ()
 {
-   	window = SDL_CreateWindow("Hello, Triangle!", 960, 540, SDL_WINDOW_RESIZABLE);
-	device = SDL_CreateGPUDevice(SDL_GPU_SHADERFORMAT_SPIRV, false, NULL);
-    	
-	SDL_ClaimWindowForGPUDevice(device, window);
-    	
+	initWindow();
+	while(gameRunning)
+	{
+		SDL_Event event;
+		while(SDL_PollEvent(&event))
+		{
+			switch(event.type)
+			{
+				case SDL_EVENT_QUIT:
+					SDL_Log("quit event");
+					gameRunning = 0;
+					break;
+			}
+		}
+
+	}
+	    	
     	return 0;
 }
 
